1690d73e8861d9fb31f05db12691611d6a925852
tmarchia <tmarchia@stevens.edu>
2020-11-18 18:20:13 -0500

    adding completed HW10

--- Student_Repository_TylerMarchiano.py
+++ Student_Repository_TylerMarchiano.py
@@ -16,7 +16,7 @@ class Student:
         '''Stores information about a single student'''
         self.cwid: str = cwid
         self.name: str = name
-        self.major: str = major
+        self.major: Major = major
         self.courses: Dict[str, str] = dict() #Key is course and value is the grade
     
     def add_course(self, course: str, grade: str) -> None:
@@ -42,8 +42,12 @@ class Student:
         for course, grade in self.courses.items():
             total_value += grade_values[grade]
             num_courses +=1
-    
-        return round(total_value/num_courses,2)
+
+        try:
+            gpa: float = round(total_value/num_courses,2)
+        except:
+            raise ZeroDivisionError ("Can't divide by 0")
+        return gpa
 
 class Instructor:
     def __init__(self, cwid: str, name: str, department: str) -> None:
@@ -60,16 +64,57 @@ class Instructor:
         #add the course to the dictionary if it doesn't exist already and increment the count by one
         self.courses[course] += 1
 
+class Major:
+    def __init__(self, major: str) -> None:
+        '''Stores information about one major'''
+        self.name: str = major
+        self.required_courses : List[str] = list()
+        self.electives : List[str] = list()
+    
+    def add_course(self, flag: str, course: str) -> None:
+        if flag == 'R':
+            if course not in self.required_courses:
+                self.required_courses.append(course)
+        else:
+            if course not in self.electives:
+                self.electives.append(course)
+    
+    def get_student_remaining_required(self, student: Student) -> List[str]:
+        '''return a list of the required courses the student has taken'''
+        completed_courses: List[str] = student.compute_completed_courses()
+        remaining_required: List[str] = list()
+        for course in self.required_courses:
+            if course not in completed_courses:
+                remaining_required.append(course)
+        
+        return remaining_required
+
+    def get_student_remaining_electives(self, student: Student) -> List[str]:
+        '''return a list of the required courses the student has taken'''
+        completed_courses: List[str] = student.compute_completed_courses()
+        completed_elective: bool = False
+        for course in completed_courses:
+            if course in self.electives:
+                completed_elective = True
+        
+        if completed_elective:
+            return list()
+        else:
+            return self.electives
+
 class University:
     '''Holds all of the data for a specific University'''
     students: Dict[str, Student] = dict() #key is cwid and the value is the student. This will make it easier to find students later
     instructors: Dict[str, Instructor] = dict() #key is cwid and the value is the Instructor. This will make it easier to find instructors later
+    majors: Dict[str, Major] = dict() #key is major name and the value is the Major object
     
     def __init__(self, path: str) -> None:
         self.path: str = path
+        self.read_majors()
         self.read_students()
         self.read_instructors()
         self.read_grades()
+        self.majors_prettytable()
         self.student_prettytable()
         self.instructor_prettytable()
     
@@ -82,7 +127,7 @@ class University:
             raise FileNotFoundError(f"Can't find students.txt in the given directory")
         
         for cwid, name, major in students_file:
-            self.students[cwid] = Student(cwid, name, major)
+            self.students[cwid] = Student(cwid, name, self.majors[major])
     
     def read_instructors(self) -> None:
         '''read the instructors.txt file, creating a new Student for each line in the file'''
@@ -111,11 +156,43 @@ class University:
             #add the course to the professor if it doesn't exist already and increment the count of the students
             self.instructors[p_cwid].add_course(course)
     
+    def read_majors(self) -> None:
+        '''reads the majors.txt file and processes all of that information. 
+        Create a new object of type Major for each major'''
+        file_path: str = path.join(self.path, 'majors.txt')
+        try:
+            majors_file: List[str] = list(file_reader(file_path, 3, sep='\t', header=True))
+        except:
+            raise FileNotFoundError(f"Can't find majors.txt in the given directory")
+        
+        for major, flag, course in majors_file:
+            if major not in self.majors:
+                self.majors[major] = Major(major)
+                self.majors[major].add_course(flag, course)
+            else:
+                self.majors[major].add_course(flag, course)
+
+    def majors_prettytable(self) -> None:
+        '''print the mjor pretty table '''
+        pt: PrettyTable = PrettyTable(field_names=['Major', 'Required Courses', 'Electives'])
+        for major in self.majors.values():
+            pt.add_row([major.name, sorted(major.required_courses), sorted(major.electives)])
+        print()
+        print("Majors Summary: ")
+        print(pt)
+        
     def student_prettytable(self) -> None:
         '''print the student pretty table'''
-        pt: PrettyTable = PrettyTable(field_names=["CWID", "Name", "Completed Courses", "GPA"])
+        pt: PrettyTable = PrettyTable(field_names=["CWID", "Name", "Major", "Completed Courses", "Remaining Required", "Remaining Electives", "GPA"])
+        
         for student in self.students.values():
-            pt.add_row([student.cwid, student.name, sorted(student.compute_completed_courses()), student.calculate_gpa()])
+            completed_courses: List[str] = sorted(student.compute_completed_courses())
+            remaining_electives: List[str] = sorted(student.major.get_student_remaining_electives(student))
+            remaining_required: List[str] = sorted(student.major.get_student_remaining_required(student))
+            
+            pt.add_row([student.cwid, student.name, student.major.name, completed_courses, remaining_required, remaining_electives, student.calculate_gpa()])
+        
+        print()
         print("Student Summary: ")
         print(pt)
     
@@ -125,6 +202,7 @@ class University:
         for instructor in self.instructors.values():
             for course in instructor.courses:
                 pt.add_row([instructor.cwid, instructor.name, instructor.department, course, instructor.courses[course]])
+        print()
         print("Instructor Summary: ")
         print(pt)
         
@@ -144,7 +222,7 @@ def main() -> None:
         
         if response.lower() == 'no':
             runAgain = False
-
+    
 if __name__ == "__main__":
     main()
 
